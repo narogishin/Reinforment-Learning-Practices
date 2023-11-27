425d61558836b50ab4ec35074d4124b12da8cf7b
narogishin <tdoctor109@gmail.com>
2023-11-27 22:15:06 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import gym
+import gymnasium as gym
+import numpy as np
+import matplotlib.pyplot as plt
 
-env = gym.make("MountainCar-v0", render_mode="rgb_array")
-state = env.reset()
-
+env = gym.make('MountainCarContinuous-v0', render_mode="rgb_array")
+env.reset()
 done = False
 
-action = 2
-new_state, reward, done, info, _ = env.step(action)
-env.render()
+while not done:
+    action = np.array([2])
+    new_state, reward, done, info, _ = env.step(action)
+    env.render()
 
 env.close()
