81f90457c6ef73cf4c1d71c7cd170e6e9f595a61
narogishin <tdoctor109@gmail.com>
2023-12-05 22:27:46 +0100

    modified:   main.py 	new file:   requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import gymnasium as gym
 import numpy as np
 
-env = gym.make('MountainCarContinuous-v0', render_mode="human")
+env = gym.make('MountainCar-v0', render_mode="human")
 env.reset()
 
+LEARNING_RATE = 0.1
+DISCOUNT = 0.95
 Action_SPACE = 3
+
 DISCRETE_OS_SIZE = [20]*len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low)/DISCRETE_OS_SIZE
 
@@ -13,7 +16,7 @@ q_table = np.random.uniform(low=-2, high=0, size=DISCRETE_OS_SIZE+[Action_SPACE]
 done = False
 
 while not done:
-    action = np.array([2])
+    action = 2
     new_state, reward, done, info, _ = env.step(action)
 
 env.close()
