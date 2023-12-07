d9703010e66cc46249e954923df9b3e851d4780a
narogishin <tdoctor109@gmail.com>
2023-12-07 21:53:25 +0100

    modified:   main.py     started learning

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ Action_SPACE = 3
 EPISODES = 2500
 SHOW_EVERY = 200
 
+
 def get_discrete_state(state):
     discrete_state = (state - env.observation_space.low)/discrete_os_win_size
-    return tuple(discrete_state.astype(np.int))
+    return tuple(discrete_state.astype(np.int64))
 
 DISCRETE_OS_SIZE = [20]*len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low)/DISCRETE_OS_SIZE
@@ -21,7 +22,7 @@ q_table = np.random.uniform(low=-2, high=0, size=DISCRETE_OS_SIZE+[Action_SPACE]
 for episode in range(EPISODES):
     if episode % SHOW_EVERY == 0:
         render = True
-        print(episode)
+        # print(episode)
     else:
         render  = False
 
@@ -31,15 +32,17 @@ for episode in range(EPISODES):
         action = np.argmax(q_table[discrete_state])
         new_state, reward, done, info, _ = env.step(action)
         new_discrete_state = get_discrete_state(new_state)
+        
         if render:
             env.render()
+
         if not done:
             max_future_q = np.max(q_table[new_discrete_state])
             current_q = q_table[discrete_state+(action,)]
             new_q = (1-LEARNING_RATE)*current_q + LEARNING_RATE*(reward + DISCOUNT*max_future_q)
             q_table[discrete_state+(action,)] = new_q
 
-        elif new_state >= env.goal_position:
+        elif new_state[0] >= env.goal_position:
             q_table[discrete_state+(action,)] = 0
 
         discrete_state = new_discrete_state
